e5407741c217209aff855c6cadaf2509f4170282
tyrantsMedic <hamzaofe786@hotmail.com>
2023-10-03 15:04:42 +0500

    Completed

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-    const addNewExpenseHandler = newExpenseData => props.onAddExpense(newExpenseData);
+  const [showExpenseform, setShowExpenseForm] = useState(false);
+
+  const addNewExpenseHandler = newExpenseData => {
+    props.onAddExpense(newExpenseData);
+    setShowExpenseForm(false);
+  }
+
+  const formCancelHandler = () => setShowExpenseForm(false);
+  const displayFormHandler = () => setShowExpenseForm(true);
+
   return (
     <div className="new-expense">
-      <ExpenseForm onAddNewExpense={addNewExpenseHandler} />
+      {!showExpenseform ? (
+        <button onClick={displayFormHandler}>Add new expense</button>
+      ) : (
+        <ExpenseForm onAddNewExpense={addNewExpenseHandler} onCancel={formCancelHandler} />
+      )}
     </div>
   );
 };
--- src/components/expenses/ExpenseComponent.js
+++ src/components/expenses/ExpenseComponent.js
@@ -3,11 +3,15 @@ import ExpensesFilter from "./ExpenseFilter";
 import "./ExpenseComponent.css";
 import Card from "../UI/Card";
 import { useState } from "react";
+import ExpensesChart from "./ExpensesChart";
 
 const ExpenseComponent = (props) => {
   const [selectedFilter, setSelectedFilter] = useState("2020");
-  const selectedFilterHandler = (filter) => setSelectedFilter(filter);
-  let expenseFound = false;
+  const selectedFilterHandler = filter => setSelectedFilter(filter);
+
+  const filteredExpenses = props.expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === selectedFilter
+  });
 
   return (
     <Card className="expenses">
@@ -15,16 +19,13 @@ const ExpenseComponent = (props) => {
         selected={selectedFilter}
         onSelectedFilter={selectedFilterHandler}
       />
+      <ExpensesChart expenses={filteredExpenses}/>
       <ul className="expenses-list">
-        {props.expenses.map((expense) => {
-          if (expense.date.getFullYear().toString() === selectedFilter) {
-            expenseFound = true;
-            return <ExpenseItem key={expense.id} expense={expense} />;
-          }
-          return null;
+        {filteredExpenses.map((expense) => {
+          return <ExpenseItem key={expense.id} expense={expense} />;
         })}
       </ul>
-      {expenseFound === false && (
+      {filteredExpenses.length === 0 && (
         <h2 className="expenses-list__fallback">No Expense Found</h2>
       )}
     </Card>
